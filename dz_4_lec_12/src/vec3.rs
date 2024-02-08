39a6e3ded710090e5031b6c8cac294c5ea5e5097
OD <odemina14.02@gmail.com>
2024-02-08 21:22:13 +0300

    dz_4_lec_12

---
+++ dz_4_lec_12/src/vec3.rs
@@ -0,0 +1,38 @@
+pub const VEC3_LEN: usize = 3;
+
+#[derive(Debug, PartialEq)]
+pub struct Vec3([i32; VEC3_LEN]);
+
+impl Vec3 {
+    pub fn default() -> Vec3 {
+        Vec3([0; 3])
+    }
+    
+    pub fn scalar_sum(a: Vec3, b: Vec3) -> i32 {
+        let mut c = 0;
+        for i in 0..VEC3_LEN {
+            c += a.0[i] + b.0[i];
+        }
+        c
+    }
+    
+    pub fn vector_sum(a: Vec3, b: Vec3) -> Vec3 {
+        let mut c = Vec3::default();
+        for i in 0..3 {
+            c.0[i] = a.0[i] + b.0[i];
+        }
+        c
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn vec3() {
+        assert_eq!(Vec3::default(), Vec3([0; 3]));
+        assert_eq!(Vec3::scalar_sum(Vec3([5; 3]), Vec3([2; 3])), 21);
+        assert_eq!(Vec3::vector_sum(Vec3([5; 3]), Vec3([2; 3])), Vec3([7; 3]));
+    }
+}
